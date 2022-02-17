f2f277becf840d9c42586c77cd9540d495e6602d
Nikhikande <91787212+Nikhikande@users.noreply.github.com>
2022-02-17 21:13:51 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./routes";
+import Routes from "./Routes";
 
 ReactDOM.render(
   <React.StrictMode>
